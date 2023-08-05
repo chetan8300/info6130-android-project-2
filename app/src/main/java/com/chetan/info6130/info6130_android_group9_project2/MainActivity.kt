6e1a73af9162c7b58451c25120f6410e553c7d4e
Chetan Godhani <chetangodhani9@gmail.com>
2023-08-04 20:39:03 -0400

    Initial Commit

---
+++ app/src/main/java/com/chetan/info6130/info6130_android_group9_project2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chetan.info6130.info6130_android_group9_project2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
